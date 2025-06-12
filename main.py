cd21fb59aafb3bbbbddb05d2f3b5caf0285595ae
Chris Groom <christophergroom@yahoo.com>
2025-06-12 11:16:22 +0100

    Add cattle endpoints

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Vitulo Backend is running"}
+# In-memory database
+cattle_db = []
+
+# Pydantic model
+class Cattle(BaseModel):
+    id: int
+    tag_number: str
+    breed: str
+    purchase_price: float
+    farm: str
+    sex: str
+    dob: str
+
+@app.post("/cattle/", response_model=Cattle)
+def add_cattle(cow: Cattle):
+    cattle_db.append(cow)
+    return cow
+
+@app.get("/cattle/", response_model=List[Cattle])
+def get_all_cattle():
+    return cattle_db
+
