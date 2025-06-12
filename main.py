21fc8bcdfe3cb692ee7d13d94903c27f9173a225
Chris Groom <christophergroom@yahoo.com>
2025-06-12 12:05:52 +0100

    Merge new backend features

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
+
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import cattle
 
 app = FastAPI()
 
-# In-memory database
-cattle_db = []
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Pydantic model
-class Cattle(BaseModel):
-    id: int
-    tag_number: str
-    breed: str
-    purchase_price: float
-    farm: str
-    sex: str
-    dob: str
-
-@app.post("/cattle/", response_model=Cattle)
-def add_cattle(cow: Cattle):
-    cattle_db.append(cow)
-    return cow
-
-@app.get("/cattle/", response_model=List[Cattle])
-def get_all_cattle():
-    return cattle_db
-
+app.include_router(cattle.router)
